10fec153ec9c76eecc5175b86ac1ad9469f73173
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2026-03-26 23:16:34 +0600

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,20 @@ import { app, server } from "./lib/socket.js";
 app.use(express.json({ limit: "10 mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chatting-application-63as.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true
+  origin: function(origin, callback){
+    if(!origin || allowedOrigins.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error("CORS blocked"));
+    }
+  },
+  credentials: true
 }));
 
 app.use("/api/auth", authRoute);
